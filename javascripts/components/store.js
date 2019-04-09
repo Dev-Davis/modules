accdd850a4fd1269f2b5d5d02d79ef04cdad738b
Dev Davis <sean.dev.davis@gmail.com>
2019-04-09 18:54:46 -0500

    added to store.js and modified main.css

--- javascripts/components/store.js
+++ javascripts/components/store.js
@@ -1,13 +1,21 @@
 import book from '../helpers/book.js'
 import util from './../helpers/util.js';
 
+const addToCartEvent = (e) => {
+    e.preventDefault();
+    const bookInfo = book.getBook();
+    console.log('book:', bookInfo);
+};
+
 const makeStore = () => {
     const bookInfo = book.getBook();
     let domString = `<h2>Our Only Book!</h2>`;
     domString += `<h3>${bookInfo.price}</h3>`;
-    domString += `<img src="${bookInfo.image}" alt="50 shades of chicken book cover"/>`;
-    domString += `<button class="btn btn-primary>Add to Cart</button>`;
+    domString += `<img class="shadesOfChicken" src="${bookInfo.image}" alt="50 shades of chicken book cover"/>`;
+    // domString += `<br><br>`
+    domString += `<button id="cartBtn" class="btn btn-primary" class="addBtn">Add To Cart</button>`;
     util.printToDom('store-container', domString);
+    document.getElementById('cartBtn').addEventListener('click', addToCartEvent);
 };
 
 export default { makeStore };
